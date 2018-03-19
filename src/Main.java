084d66cf68a2a57ee0794ef03366881767ff6a18
Creselia <valeria.sterzhanova@gmail.com>
2018-03-19 23:35:14 +0300

    implement reading from .txt file

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
+    private static List<String> lines;
+
+    public static void main(String[] args) throws IOException {
+        getFile();
+        for (String s:lines) {
+            System.out.println(s);
+            System.out.println();
+        }
+    }
+
+    private static void getFile() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            Path path = Paths.get("C:\\Users\\u1\\Desktop\\abcd.txt");
+            lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            System.out.println("File not found or Can't read file");
+        }
+
+        reader.close();
+    }
 }
